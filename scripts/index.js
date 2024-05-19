9260ebfd31fec3852262d6eb38215f23bf6598f6
Елена Тулина <alenka@MacBook-Air-Elena.local>
2024-05-20 00:27:16 +0500

    test3

--- scripts/index.js
+++ scripts/index.js
@@ -5,28 +5,24 @@ const template = document.querySelector('#card-template');
 // @todo: DOM узлы
 const sectionElementPlaces = document.querySelector('.places');
 const conteinerCards = sectionElementPlaces.querySelector('.places__list');
+
 // @todo: Функция создания карточки
-function createCard(cardData) {
-    const cardHtml = template.content.cloneNode(true);
+function createCard(cardData, deleteCallback) {
+    const cardHtml = template.content.firstElementChild.cloneNode(true);
+    // firstElementChild - первый дочерний элемент в document-fragment
     cardHtml.querySelector('.card__title').textContent = cardData.name;
     cardHtml.querySelector('.card__image').src = cardData.link;
     cardHtml.querySelector('.card__image').alt = cardData.name;
-    cardHtml.querySelector('.card__delete-button').addEventListener('click', function () {
-        card.remove(); 
-    })
+    cardHtml.querySelector('.card__delete-button').addEventListener('click',() => { deleteCallback(cardHtml);
+ });
 return cardHtml;
 };
-// do: Функция удаления карточки
-list.addEventListener('click', function (event) {
-    const isDeleteButton = event.target.classList.contains('card__delete-button');
-    if (isDeleteButton) {
-       const card = event.target.closest('.card');
-       card.remove();
-    }
-});
-
+// do: Функция удаления карточки 
+function deleteCard(card) {
+    card.remove()
+}
 // @todo: Вывести карточки на страницу
 initialCards.forEach((cardData) => {
-    const cardHtml = createCard(cardData);
+    const cardHtml = createCard(cardData, deleteCard);
     list.append(cardHtml);
- })
+})
